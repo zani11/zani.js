0ef6982df1eebd9bd1f08808f381c37f16f76b9f
zani11 <162561527+zani11@users.noreply.github.com>
2024-03-07 15:38:01 +0530

    Add files via upload

---
+++ file2.js
@@ -0,0 +1,24 @@
+// into variable
+
+// variable can store some information
+//we can use that information later
+// we can change that information later
+
+// declare a variable
+
+//use a variable
+
+// change value
+
+
+// var firstname = "amaan2";
+
+// console.log(firstname);
+
+
+var firstname = "amaan";
+console.log(firstname);
+
+
+var firstname = "zani";
+console.log(firstname);
